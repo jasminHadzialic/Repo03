f8c19cd5657d6d8d3091502fa340e73f6440dd9e
Jasmin Hadzialic <46654927+jasminHadzialic@users.noreply.github.com>
2021-09-16 10:55:06 +0200

    drugi

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    // Ovo je komentar jedne linije
+    /* Ovo je vise 
+    linijski komentar*/
+    // Only change code above this line
+}
+
+main();
+
+module.exports = main;
